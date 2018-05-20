a3c78328c386f70044d0b880a6c63aeb3c21b8e4
Peter Wirdemo <peter.wirdemo@gmail.com>
2018-05-20 14:07:43 +0200

    Added debug and tested on mint

--- kernel.pl
+++ kernel.pl
@@ -1,17 +1,29 @@
 #!/usr/bin/perl -w
 
 use strict;
+my($debug) = 1;
 
 # tested ok on swift: DISTRIB_DESCRIPTION="Ubuntu 16.04.4 LTS"
+# tested ok on dev: DISTRIB_DESCRIPTION="Linux Mint 18.1 Serena"
+
 # kernel=        linux	/vmlinuz-4.13.0-41-generic root=/dev/mapper/xubuntu--vg-root ro  quiet splash $vt_handoff
 my($grubconf) = "/boot/grub/grub.cfg";
 
 # version=Linux version 4.13.0-41-generic (buildd@lgw01-amd64-028) (gcc version 5.4.0 20160609 (Ubuntu 5.4.0-6ubuntu1~16.04.9)) #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018
 my($procver) = "/proc/version";
 
+sub debug($) {
+	return unless ( $debug );
+	my($str) = shift;
+	chomp($str);
+	print "DEBUG: $str\n";
+}
+
+
 sub readfile($) {
 	my($file) = shift;
 
+	debug("Reading file $file");
 	unless ( open(IN,"<$file") ) {
 		die "Reading $file: $!\n";
 	}
@@ -35,21 +47,26 @@ foreach ( @grub ) {
 	# $_="linux	/vmlinuz-4.13.0-41-generic root=/dev/mapper/xubuntu--vg-root ro  quiet splash $vt_handoff"
 
 	next unless ( m/^linux\s+/ );
+	debug("Got something with linux: \"$_\"");
+
 	s/^linux\s+//;
+	debug("Removed initial linux+space: \"$_\"");
 	# $_="/vmlinuz-4.13.0-41-generic root=/dev/mapper/xubuntu--vg-root ro  quiet splash $vt_handoff"
 
 	($kernel) = split(/\s+/,$_);
+	debug("Extract first field: \"$kernel\"");
 	# $kernel="/vmlinuz-4.13.0-41-generic"
 
 	next unless ( $kernel );
 
 	$kernel =~ s/^\D+-//;
+	debug("Removed starting characters: \"$kernel\"");
 	# $kernel="/4.13.0-41-generic"
 	
 	$kernel =~ s/-\D+$//;
+	debug("Removed ending characters: \"$kernel\"");
 	# $kernel="4.13.0-41"
 
-	print "DEBUG: kernel=$kernel\n";
 	last;
 }
 die "Could not find kernel in $grubconf, exiting...\n" unless ($kernel);
@@ -57,13 +74,24 @@ die "Could not find kernel in $grubconf, exiting...\n" unless ($kernel);
 
 my(@procver) = readfile($procver);
 my($version) = shift(@procver);
+debug("Retrieved version: \"$version\"");
+# version=Linux version 4.13.0-41-generic (buildd@lgw01-amd64-028) (gcc version 5.4.0 20160609 (Ubuntu 5.4.0-6ubuntu1~16.04.9)) #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018
 
-print "version=$version\n";
-print "kernel=$kernel\n";
+$version =~ s/^\D+//;
+debug("Removed starting characters: \"$version\"");
+# version=4.13.0-41-generic (buildd@lgw01-amd64-028) (gcc version 5.4.0 20160609 (Ubuntu 5.4.0-6ubuntu1~16.04.9)) #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018
+
+($version) = split(/\s/,$version);
+debug("Extract first field: \"$version\"");
+# version=4.13.0-41-generic 
+
+$version =~ s/-\D+$//;
+debug("Removed ending characters: \"$version\"");
+# version=4.13.0-41 
 
 if ( $version =~ /$kernel/ ) {
 	print "Running latest($kernel) installed kernel\n";
 }
 else {
-	print "Not running latest($kernel) installed kernel\n";
+	print "Running old($version) instead of running latest($kernel) installed kernel\n";
 }
