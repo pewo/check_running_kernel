7abc8c2ebab0551997424d1587235e4186f1fcf8
Peter Wirdemo <peter.wirdemo@gmail.com>
2018-06-02 21:17:33 +0200

    added deleted.pl

---
+++ deleted.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+
+my($pid);
+foreach $pid ( </proc/*> ) {
+	my($fd) = $pid . "/map_files";
+	next unless ( -d $fd );
+	my(@arr) = ();
+	foreach ( <$fd/*> ) {
+		next unless ( -l $_ );
+		my($dest) = readlink($_);
+		next unless ( defined($dest) );
+		next if ( $dest =~ /\/dev\// );
+		if ( $dest =~ /(\s+\(.*\))/ ) {
+			$dest =~ s/\s+\(.*\)//;
+			next unless ( -r $dest );
+			push(@arr,$dest);
+		}
+	}
+	if ( $#arr >= 0 ) {
+		my($cmdline) = "$pid/cmdline";
+		next unless ( -r $cmdline );
+		next unless ( open(IN,"<$cmdline") );
+		my($cmd) = <IN>;
+		print "***\n$cmd $pid \n";
+		foreach ( @arr ) {
+			print "\t$_\n";
+		}
+	}
+}
